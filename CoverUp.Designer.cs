542b31dffcba67b076c526df1929d118e770b619
Albert G Carrick <gilcarrick@gmail.com>
2021-10-17 15:56:26 -0500

    Add project files.

---
+++ CoverUp.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace CoverUp
+{
+    partial class CoverUp
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Color = new System.Windows.Forms.ToolStripMenuItem();
+            this.OnTop = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Color,
+            this.OnTop});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(282, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Color
+            // 
+            this.Color.Name = "Color";
+            this.Color.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.C)));
+            this.Color.Size = new System.Drawing.Size(59, 24);
+            this.Color.Text = "Color";
+            this.Color.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Color.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // OnTop
+            // 
+            this.OnTop.Name = "OnTop";
+            this.OnTop.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.T)));
+            this.OnTop.Size = new System.Drawing.Size(67, 24);
+            this.OnTop.Text = "OnTop";
+            this.OnTop.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.OnTop.Click += new System.EventHandler(this.OnTop_Click);
+            // 
+            // CoverUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 53);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(300, 80);
+            this.Name = "CoverUp";
+            this.Text = "CoverUp";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CoverUp_FormClosing);
+            this.Load += new System.EventHandler(this.CoverUp_FormLoad);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Color;
+        private System.Windows.Forms.ToolStripMenuItem OnTop;
+    }
+}
+
---
+++ CoverUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoverUp.Properties;
+
+namespace CoverUp
+{
+    public partial class CoverUp : Form
+    {
+        private object CoreApplication;
+
+        public CoverUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Color_Click(object sender, EventArgs e)
+        {
+            //var coreTitleBar = new bar;
+            if (this.BackColor == System.Drawing.Color.Black)
+            {
+                this.BackColor = System.Drawing.Color.White;
+                
+                this.Color.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                this.BackColor = System.Drawing.Color.Black;
+                this.Color.BackColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void OnTop_Click(object sender, EventArgs e)
+        {
+            if (this.TopMost == true)
+                this.TopMost = false;
+            else this.TopMost = true;
+        }
+
+        private void CoverUp_FormLoad(object sender, EventArgs e)
+        {
+            this.Location = Settings.Default.WindowLocation;        
+            this.Size = Settings.Default.WindowSize;
+        }
+
+        private void CoverUp_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Copy window location to app settings
+            Settings.Default.WindowLocation = this.Location;
+            // Copy window size to app settings
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Settings.Default.WindowSize = this.Size;
+            }
+            else
+            {
+                Settings.Default.WindowSize = this.RestoreBounds.Size;
+            }
+            // Save settings
+            Settings.Default.Save();
+        }
+    }
+}
